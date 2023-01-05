c55868e33f9f86c24b2adca1dce4f15e2ac94619
ebecerra00 <106988018+ebecerra00@users.noreply.github.com>
2023-01-05 15:11:30 -0500

    Assignment 11

---
+++ BecerraEstevan_Assignment11/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace BecerraEstevan_Assignment11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTest = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblGPA = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtGPA = new System.Windows.Forms.TextBox();
+            this.grpFocus = new System.Windows.Forms.GroupBox();
+            this.radSystem = new System.Windows.Forms.RadioButton();
+            this.radCyber = new System.Windows.Forms.RadioButton();
+            this.radNET = new System.Windows.Forms.RadioButton();
+            this.radCIT = new System.Windows.Forms.RadioButton();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lstShow = new System.Windows.Forms.ListBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.grpFocus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(92, 9);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(62, 13);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "Test File 10";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(32, 35);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // lblGPA
+            // 
+            this.lblGPA.AutoSize = true;
+            this.lblGPA.Location = new System.Drawing.Point(32, 61);
+            this.lblGPA.Name = "lblGPA";
+            this.lblGPA.Size = new System.Drawing.Size(32, 13);
+            this.lblGPA.TabIndex = 2;
+            this.lblGPA.Text = "GPA:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(80, 32);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtGPA
+            // 
+            this.txtGPA.Location = new System.Drawing.Point(80, 58);
+            this.txtGPA.Name = "txtGPA";
+            this.txtGPA.Size = new System.Drawing.Size(100, 20);
+            this.txtGPA.TabIndex = 2;
+            // 
+            // grpFocus
+            // 
+            this.grpFocus.Controls.Add(this.radSystem);
+            this.grpFocus.Controls.Add(this.radCyber);
+            this.grpFocus.Controls.Add(this.radNET);
+            this.grpFocus.Controls.Add(this.radCIT);
+            this.grpFocus.Location = new System.Drawing.Point(186, 9);
+            this.grpFocus.Name = "grpFocus";
+            this.grpFocus.Size = new System.Drawing.Size(120, 113);
+            this.grpFocus.TabIndex = 5;
+            this.grpFocus.TabStop = false;
+            this.grpFocus.Text = "Focus";
+            // 
+            // radSystem
+            // 
+            this.radSystem.AutoSize = true;
+            this.radSystem.Location = new System.Drawing.Point(21, 88);
+            this.radSystem.Name = "radSystem";
+            this.radSystem.Size = new System.Drawing.Size(59, 17);
+            this.radSystem.TabIndex = 3;
+            this.radSystem.TabStop = true;
+            this.radSystem.Text = "System";
+            this.radSystem.UseVisualStyleBackColor = true;
+            // 
+            // radCyber
+            // 
+            this.radCyber.AutoSize = true;
+            this.radCyber.Location = new System.Drawing.Point(21, 65);
+            this.radCyber.Name = "radCyber";
+            this.radCyber.Size = new System.Drawing.Size(88, 17);
+            this.radCyber.TabIndex = 2;
+            this.radCyber.TabStop = true;
+            this.radCyber.Text = "Cybersecurity";
+            this.radCyber.UseVisualStyleBackColor = true;
+            // 
+            // radNET
+            // 
+            this.radNET.AutoSize = true;
+            this.radNET.Location = new System.Drawing.Point(21, 42);
+            this.radNET.Name = "radNET";
+            this.radNET.Size = new System.Drawing.Size(47, 17);
+            this.radNET.TabIndex = 1;
+            this.radNET.TabStop = true;
+            this.radNET.Text = "NET";
+            this.radNET.UseVisualStyleBackColor = true;
+            // 
+            // radCIT
+            // 
+            this.radCIT.AutoSize = true;
+            this.radCIT.Location = new System.Drawing.Point(21, 19);
+            this.radCIT.Name = "radCIT";
+            this.radCIT.Size = new System.Drawing.Size(42, 17);
+            this.radCIT.TabIndex = 0;
+            this.radCIT.TabStop = true;
+            this.radCIT.Text = "CIT";
+            this.radCIT.UseVisualStyleBackColor = true;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(95, 94);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 6;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.BtnEnter_Click);
+            // 
+            // lstShow
+            // 
+            this.lstShow.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstShow.FormattingEnabled = true;
+            this.lstShow.ItemHeight = 14;
+            this.lstShow.Location = new System.Drawing.Point(35, 123);
+            this.lstShow.Name = "lstShow";
+            this.lstShow.Size = new System.Drawing.Size(271, 88);
+            this.lstShow.TabIndex = 7;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(12, 224);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 8;
+            this.btnDisplay.Text = "&Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(93, 224);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 9;
+            this.btnAverage.Text = "&Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.BtnAverage_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(174, 224);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(255, 224);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 267);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lstShow);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.grpFocus);
+            this.Controls.Add(this.txtGPA);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblGPA);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTest);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpFocus.ResumeLayout(false);
+            this.grpFocus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblGPA;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtGPA;
+        private System.Windows.Forms.GroupBox grpFocus;
+        private System.Windows.Forms.RadioButton radSystem;
+        private System.Windows.Forms.RadioButton radCyber;
+        private System.Windows.Forms.RadioButton radNET;
+        private System.Windows.Forms.RadioButton radCIT;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox lstShow;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ BecerraEstevan_Assignment11/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace BecerraEstevan_Assignment11
+{
+    public partial class Form1 : Form
+    {
+        private const int cSize = 40;
+        private string[] mName = new string[cSize];
+        private double[] mGPA = new double[cSize];
+        private string[] mArea = new string[cSize];
+        private int mIndex = 0;
+
+        private string mFileName = Path.Combine(Application.StartupPath, "Students.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayMessageOK(string message)
+        {
+            MessageBox.Show(message, Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private bool ValidateInputs()
+        {
+            double GPA;
+            
+            if(txtName.Text =="")
+            {
+                DisplayMessageOK("Please enter name.");
+                return false;
+            }
+
+            if (double.TryParse(txtGPA.Text, out GPA) == false)
+            {
+                DisplayMessageOK("GPA must be a whole number");
+                return false;
+            }
+
+            if (GPA < 0 || GPA > 4.0)
+            {
+                DisplayMessageOK("GPA must be between 0 and 4");
+                txtGPA.Focus();
+                return false;
+            }
+
+            if(radCIT.Checked == false && radCyber.Checked == false && radNET.Checked == false && radSystem.Checked == false)
+            {
+                DisplayMessageOK("Must select radio button");
+                return false;
+            }
+
+            return true;
+        }
+
+        private double ComputeAvg(double[] data, int count)
+        {
+            double average;
+            double sum = 0;
+
+
+            for (int k = 0; k < mIndex; k++)
+            {
+                sum = sum + mGPA[k];
+            }
+
+            average = sum / mIndex; //calculate the average
+            return average;
+
+        }
+
+        private void ClearInputs()
+        {
+            txtName.Clear();
+            txtGPA.Clear();
+            lstShow.Items.Clear();
+            radCIT.Checked = false;
+            radCyber.Checked = false;
+            radNET.Checked = false;
+            radSystem.Checked = false;
+            txtName.Focus();
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            if(ValidateInputs()==false)
+            {
+                return;
+            }
+
+            string Name = txtName.Text;
+            double GPA = double.Parse(txtGPA.Text);
+            string Area;
+
+            if(radCIT.Checked == true)
+            {
+                Area = "CIT";
+            }
+
+            else if(radCyber.Checked == true)
+            {
+                Area = "Cybersecurity";
+            }
+
+            else if(radNET.Checked == true)
+            {
+                Area = "NET";
+            }
+
+            else
+            {
+                Area = "System";
+            }
+
+            mName[mIndex] = Name;
+            mGPA[mIndex] = GPA;
+            mArea[mIndex] = Area;
+            mIndex++;
+
+            if(mIndex == cSize)
+            {
+                DisplayMessageOK("Array is full");
+                btnEnter.Enabled = false;
+            }
+
+            ClearInputs();
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            if (mIndex == 0) //check if array is empty
+            {
+                DisplayMessageOK("Array is empty");
+                return;
+            }
+
+            lstShow.Items.Clear();
+
+            string Name = "Name";
+            string  Area = "Focus";
+            string GPA = "GPA";
+
+            lstShow.Items.Clear();
+
+            lstShow.Items.Add(Name.PadRight(10) + Area.PadRight(15) + GPA);
+            lstShow.Items.Add("====================================");
+
+            for (int k = 0; k < mIndex; k++)
+            {
+                lstShow.Items.Add(mName[k].PadRight(10) + mArea[k].ToString().PadRight(15) + mGPA[k]);
+            }
+
+        }
+
+        private void BtnAverage_Click(object sender, EventArgs e)
+        {
+            double Average;
+
+            if(mIndex == 0)
+            {
+                DisplayMessageOK("Array is empty");
+                return;
+            }
+
+            lstShow.Items.Clear();
+
+            lstShow.Items.Add("Number of students entered: " + mIndex);
+
+            Average = ComputeAvg(mGPA, mIndex);
+
+            lstShow.Items.Add("Average: " + Average.ToString("N2"));
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ClearInputs();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            if (File.Exists(mFileName) == false)
+            {
+                DisplayMessageOK("There is no input file.");
+                return;
+            }
+
+            StreamReader SR = null;
+
+            string line;
+            string[] parts = new string[3];
+
+            try
+            {
+                SR = new StreamReader(mFileName);
+
+                while (SR.Peek() != -1)
+                {
+
+                    line = SR.ReadLine();
+
+                    parts = line.Split('\t');
+
+                    mName[mIndex] = parts[0];
+                    mArea[mIndex] = parts[1];
+                    mGPA[mIndex] = double.Parse(parts[2]);
+
+                    mIndex++;
+
+                    if (mIndex == cSize)
+                    {
+                        btnEnter.Enabled = false;
+                        break;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+
+
+                if (SR != null)
+                {
+                    SR.Close();
+                }
+                
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            StreamWriter writer = null;
+
+            if (MessageBox.Show("Are you sure you want to close the program?", Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            try
+            {
+                
+                int i;
+                writer = new StreamWriter(mFileName);
+
+                for (i = 0; i < mIndex; i++)
+                {
+                    writer.WriteLine(mName[i] + "\t" + mArea[i] + "\t" + mGPA[i]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                          
+                    writer.Close();
+                
+            }
+        }
+    }
+}
+    
+
