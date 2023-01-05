bc1a7a7e50334e08dfd878cdddb3230c0eb3260b
ebecerra00 <106988018+ebecerra00@users.noreply.github.com>
2023-01-05 15:10:37 -0500

    Assignment 01

---
+++ BecerraEstevan_Assignment01/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace BecerraEstevan_Assignment_081
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnStats = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.lstShow = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(121, 64);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 0;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(40, 64);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.BtnEnter_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(171, 167);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(90, 167);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnStats
+            // 
+            this.btnStats.Location = new System.Drawing.Point(9, 167);
+            this.btnStats.Name = "btnStats";
+            this.btnStats.Size = new System.Drawing.Size(75, 23);
+            this.btnStats.TabIndex = 4;
+            this.btnStats.Text = "Stats";
+            this.btnStats.UseVisualStyleBackColor = true;
+            this.btnStats.Click += new System.EventHandler(this.BtnStats_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(77, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(77, 38);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 6;
+            // 
+            // lstShow
+            // 
+            this.lstShow.FormattingEnabled = true;
+            this.lstShow.Location = new System.Drawing.Point(9, 93);
+            this.lstShow.Name = "lstShow";
+            this.lstShow.Size = new System.Drawing.Size(237, 69);
+            this.lstShow.TabIndex = 7;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(37, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "Name";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(37, 41);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 9;
+            this.lblScore.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 213);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lstShow);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnStats);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnStats;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.ListBox lstShow;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ BecerraEstevan_Assignment01/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BecerraEstevan_Assignment_081
+{/*=========================================================
+* Estevan Becerra
+* ebecerra@purdue.edu
+* CNIT155 Assignment08
+* Lab Section:   Fr. 1:30
+* Program Description:  This code is able to display scores and the name of 
+* the people who had this score.  It also displays the average score as well
+* as the max score.
+*
+* Academic Honesty: 
+*	I attest that this is my original work.
+*	I have not used unauthorized source code, either modified or unmodified.
+*	I have not given other fellow student(s) access to my program.
+*=========================================================== */
+
+    public partial class Form1 : Form
+    {
+        private const int cSize = 20;
+        private string[] mData1 = new string[cSize];
+        private int[] mData = new int[cSize];
+        private int mIndex = 0;
+        private int mCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayMessageOK(string message)
+        {
+            MessageBox.Show(message, Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private bool ValidateInput() //helper method for validation
+        {
+            int num;
+
+            if (txtName.Text == "") //name must exist
+            {
+                DisplayMessageOK("Name must be filled");//display message
+                return false;
+            }
+
+            if (int.TryParse(txtScore.Text, out num) == false) //score must be a real number
+            {
+                DisplayMessageOK("Score must be a whole number");//display message
+                txtScore.Focus();
+                return false;
+            }
+
+            if (num < 0 || num > 100) // score must be between 0 and 100
+            {
+                DisplayMessageOK("Score must be between 0 and 100");//display message
+                txtScore.Focus();
+                return false;
+
+            }
+
+            return true;
+        }
+
+        private void ClearInput() //this will clear the input
+        {
+            txtName.Clear();
+            txtScore.Clear();
+            lstShow.Items.Clear();
+
+        }
+
+        private string ToProperCase(string name) //will capitalize first letter of name
+        {
+
+            string proper;     // string in proper case
+            string firstLetter;
+            string rest;
+
+            firstLetter = name.Substring(0, 1);
+            rest = name.Substring(1);    // will give the resst of the straing after the first character
+            proper = firstLetter.ToUpper() + rest.ToLower();
+
+            return proper;
+
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            int num;
+            string name;
+            if (ValidateInput() == false) //validateing inputs
+            {
+                return;
+            }
+
+            name = txtName.Text;
+            name = ToProperCase(name);
+
+            num = int.Parse(txtScore.Text); //capitalize fist leter of name
+            mData[mIndex] = num;
+            mData1[mIndex] = name;
+            mIndex++;
+
+            if (mIndex == cSize)
+            {
+                DisplayMessageOK("Array is full"); //array is full
+                btnEnter.Enabled = false;
+            }
+            txtScore.Clear();
+            txtName.Clear();
+            txtName.Focus();
+            mCount++;
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            if (mIndex == 0) //array is empty
+            {
+                DisplayMessageOK("Array is empty");
+                return;
+            }
+
+            lstShow.Items.Clear(); //clear list box
+
+            lstShow.Items.Add("Name\t\tScore" + Environment.NewLine);
+            lstShow.Items.Add("---------------------------------------" + Environment.NewLine);
+
+            for (int k = 0; k < mIndex; k++)
+            {
+                lstShow.Items.Add(mData1[k] + "\t\t" + mData[k]);
+
+            }
+
+        }
+
+        private void BtnStats_Click(object sender, EventArgs e)
+        {
+            double counter = mCount;
+
+
+            if (mIndex == 0)
+            {
+                DisplayMessageOK("Array is empty");
+                return;
+            }
+
+            lstShow.Items.Clear();
+
+            lstShow.Items.Add("Number of students entered: " + counter);
+            lstShow.Items.Add(Environment.NewLine);
+
+            double average;
+            double sum = 0;
+
+
+            for (int k = 0; k < mIndex; k++)
+            {
+                sum = sum + mData[k];
+            }
+
+            average = sum / mIndex; //calculate the average
+            lstShow.Items.Add("Average: " + average.ToString("N2"));
+
+            lstShow.Items.Add(Environment.NewLine);
+
+            int max = 0;
+            int name = 0;
+
+
+            for (int k = 0; k < mIndex; k++)
+            {
+                if (mData[k] > max)
+
+                    max = mData[k];
+
+
+            }
+
+            lstShow.Items.Add("Maximum score: " + max); //display the max
+
+            lstShow.Items.Add(Environment.NewLine);
+
+            lstShow.Items.Add("Student's Name: " + mData1[name]); //display the name with the max
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
